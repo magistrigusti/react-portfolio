1564f66a3e5085bcd1d66e81424d4411719b8301
Magistry <Magistrigusti@yandex.ru>
2023-06-06 06:28:21 +0300

    0.1c

---
+++ src/try.js
@@ -0,0 +1,117 @@
+<div className="App">
+  <nav class="nav">
+    <div class="container">
+      <div class="nav-row">
+        <a class="logo" href="./index.html">
+          <strong>Demitrii</strong>
+          portfolio
+        </a>
+
+        <button class="dark-mode-btn">
+          <img class="dark-mode-btn__icon" src="./img/icons/sun.svg" alt="Ligth Mode" />
+          <img class="dark-mode-btn__icon" src="./img/icons/moon.svg" alt="Dark Mode" />
+        </button>
+
+        <ul class="nav-list">
+          <li class="nav-list__item">
+            <a class="nav-list__link nav-list__link--active" href="./index.html">Projects</a>
+          </li>
+          <li class="nav-list__item">
+            <a class="nav-list__link" href="./skills.html">Skills</a>
+          </li>
+          <li class="nav-list__item">
+            <a class="nav-list__link" href="./contacts.html">Contacts</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="header">
+    <div class="header__wrapper">
+      <h1 class="header__title">
+        <strong>Hi, my name is
+          <em>Demitrii</em>
+        </strong>
+        <br />
+        a frontend developer
+      </h1>
+      <div class="header__text">
+        <p>with passion for learning and creating.</p>
+      </div>
+      <a class="btn" href="#!">Download CV</a>
+    </div>
+  </header>
+
+  <main class="section">
+    <div class="container">
+      <h2 class="title-1">Projects</h2>
+      <ul class="projects">
+        <li class="project">
+          <a href="./project-page.html">
+            <img class="project__img" src="./img/projects/activebox.png" alt="Project Active Box" />
+            <h3 class="project__title">Active Box</h3>
+          </a>
+        </li>
+        <li class="project">
+          <a href="./project-page.html">
+            <img class="project__img" src="./img/projects/mogo.png" alt="Project MoGo" />
+            <h3 class="project__title">MoGo</h3>
+          </a>
+        </li>
+        <li class="project">
+          <a href="./project-page.html">
+            <img class="project__img" src="./img/projects/japan-travel.png" alt="Project Japan Travel" />
+            <h3 class="project__title">Travel to Japan</h3>
+          </a>
+        </li>
+        <li class="project">
+          <a href="./project-page.html">
+            <img class="project__img" src="./img/projects/gotrip.png" alt="Project Go Trip" />
+            <h3 class="project__title">Go Trip</h3>
+          </a>
+        </li>
+        <li class="project">
+          <a href="./project-page.html">
+            <img class="project__img" src="./img/projects/roll-shop.png" alt="Project Roll Shop" />
+            <h3 class="project__title">Roll Shop</h3>
+          </a>
+        </li>
+        <li class="project">
+          <a href="./project-page.html">
+            <img class="project__img" src="./img/projects/tonsonet.png" alt="Project Ton So Net" />
+            <h3 class="project__title">Ton So Net</h3>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </main>
+
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__wrapper">
+        <ul class="social">
+          <li class="social__item">
+            <a href="#!"><img src="./img/icons/vk.svg" alt="Link" /></a>
+          </li>
+          <li class="social__item">
+            <a href="#!"><img src="./img/icons/instagram.svg" alt="Link" /></a>
+          </li>
+          <li class="social__item">
+            <a href="#!"><img src="./img/icons/twitter.svg" alt="Link" /></a>
+          </li>
+          <li class="social__item">
+            <a href="#!"><img src="./img/icons/gitHub.svg" alt="Link" /></a>
+          </li>
+          <li class="social__item">
+            <a href="#!"><img src="./img/icons/linkedIn.svg" alt="Link" /></a>
+          </li>
+        </ul>
+
+        <div class="copyright">
+          <p>m@gistrigusti 2023</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
